a7eab083811b8640c088be5ed3dd4dfb6e0ea7cb
hbgbigstar-star <hbg.bigstar@gmail.com>
2026-03-18 00:07:08 +0700

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 
-const TOKEN = '8288397640:AAEFdGkEz6bmI7yWukFZoA_DcGysT0f-glw';
-const CHAT_ID = '-1003257858837';
+const TOKEN = '8570242392:AAHf_Vtnqb_Jyxe0Ze98lWvlOGs44vleYwk';
+const CHAT_ID = '-5180580045';
 
 const POST = async (req: NextRequest) => {
     try {
